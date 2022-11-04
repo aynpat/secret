3bb18741b5fc3688d229ae5e8bf2f056df141dad
aynpat <aynpat21@gmail.com>
2022-11-04 20:06:38 +0800

    modify authentication to passport

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded({extended :true}));
-
+app.use(session({
+    secret : "Our little Secret.",
+    resave : false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //create a connection to mongodb
 mongoose.connect("mongodb://127.0.0.1:27017/userDB",(err)=>{
@@ -23,11 +31,15 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-//encryption
+userSchema.plugin(passportLocalMongoose);
 
-userSchema.plugin(encrypt,{secret : process.env.SECRET, encryptedFields : ["password"]});
 //create a model
 const User = mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/',(req,res)=>{
     res.render("home");
@@ -42,37 +54,25 @@ app.get('/login',(req,res)=>{
 });
 
 app.post('/register',(req,res)=>{
-    const newUser = new User({
-        username : req.body.username,
-        password : req.body.password
-    });
 
-    newUser.save((err)=>{
-        if(!err){
-            console.log('success registration for new user');
-            res.render('secrets');
+    User.register({username : req.body.username}, req.body.password,function(err,user){
+        if(err){
+            console.log(err)
+            res.redirect('/register');
         }
         else{
-            console.log('failed to register');
-            res.render('register');
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            })
         }
-    });
+    })
+
+  
 });
 
 app.post('/login',(req,res)=>{
-    User.findOne({title : req.body.username},(err,result)=>{
-        if(result){
-            if(result.password === req.body.password){
-                res.render('secrets');
-            }
-            else{
-                res.send("login")
-            }
-        }
-        else{
-            console.log('di mahanap');     
-        }
-    });
+
+    
 });
 
 
